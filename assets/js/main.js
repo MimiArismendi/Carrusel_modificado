0f0a8dba0fa8c7527de09edf05fe5f957728477c
noemi <noemiarismendi.cc@gmail.com>
2017-05-30 14:50:17 -0500

    se agrega jQuery en la funcionalidad

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+    $(".control").click(function(){
+        target=$(this).data("target");
+        mostrarImagen(target);
+    });
+    
+    $(".previous").click(function(e){
+        e.preventDefault();
+        target = target - 1;
+        target = (target < 0) ? 3 : target;
+        mostrarImagen(target);
+    });
+    
+    $(".next").click(function(e){
+        e.preventDefault();
+        target = target + 1;
+        target = (target > 3) ? 0 : target;
+        mostrarImagen(target);
+    });
+    var mostrarImagen=function(target){
+    var lastSlide=$("div.active").removeClass("active");
+    var slide=$("div[data-slide='" + target + "']").addClass("active");
+    };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var target = 0;
+//
+//var cargarPagina = function () {
+//	// Elementos
+//	var botones = document.querySelectorAll(".control");
+//	var anterior = document.querySelector(".previous");
+//	var siguiente = document.querySelector(".next");
+//	
+//	// Eventos
+//	botones.forEach(function (boton) {
+//		boton.addEventListener("click", cambiarImagen);
+//	});
+//	anterior.addEventListener("click", anteriorImagen);
+//	siguiente.addEventListener("click", siguienteImagen);
+//};
+//
+//var cambiarImagen = function () {
+//	target = parseInt(this.dataset.target);
+//	mostrarImagen(target);
+//};
+//
+//var mostrarImagen = function (target) {
+//	var lastSlide = document.querySelector("div.active");
+//	var slide = document.querySelector("div[data-slide='" + target + "']");
+//	lastSlide.classList.remove("active");
+//	slide.classList.add("active");
+//};
+//
+//var anteriorImagen = function (e) {
+//	e.preventDefault();
+//	target = target - 1;
+//	target = (target < 0) ? 3 : target;
+//	mostrarImagen(target);
+//};
+//
+//var siguienteImagen = function (e) {
+//	e.preventDefault();
+//	target = target + 1;
+//	target = (target > 3) ? 0 : target;
+//	mostrarImagen(target);mostrarImagen(target);
+//};
+//
+//
+//
+//window.addEventListener("load", cargarPagina);
